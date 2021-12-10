75c076bc4986a39328db9b640e69de5b02b0ce5a
thangle <thangle@advn.vn>
2021-12-10 11:09:04 +0700

    TMP

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,31 @@
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
 set :application_name, 'mina_test'
-set :repository, 'git@git.advn.vn:product/distribution.advn.git'
+set :forward_agent, true     # SSH forward_agent.
+set :repository, 'git@github.com:leanhthang/mina_test.git'
 
-task :deploy_dev => :environment do
-  set :rails_env, 'production'
-  set :domain, 'localhost'
-  set :branch, 'master'
-  set :deploy_to, '/home/lat/mypj/deploy'
-
-  set :user, 'ruby'          # Username in the server to SSH to.
-  set :port, '7000'           # SSH port number.
-end
-
-task :local => :environment do
+task :deploy_local => :local_environment do
   set :rails_env, 'production'
   set :domain, 'localhost'
   set :branch, 'master'
   set :deploy_to, '/home/lat/mypj/deploy'
 
   set :user, 'lat'          # Username in the server to SSH to.
-  set :port, '3000'           # SSH port number.
-
-  # invoke :'rbenv:load'
+  set :port, '22'           # SSH port number.
 end
 
 task :local_environment do
-  invoke :'rvm:use', 'ruby-2.7.2@default'
+  invoke :'rvm:use', 'ruby-2.7.2p137@default'
+end
+
+task :remote_environment do
+  invoke :'rvm:use', 'ruby-2.7.2p137@default'
 end
 
 # Optional settings:
-set :forward_agent, true     # SSH forward_agent.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/uploads', 'storage')
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
@@ -41,7 +33,7 @@ set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
   # command %{rbenv install 2.7.2 --skip-existing}
-  command %{rvm install ruby-2.7.2}
+  command %{rvm install ruby-2.7.2p137@default}
   command %{gem install bundler}
 end
 
@@ -52,6 +44,8 @@ task :deploy do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
+
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
